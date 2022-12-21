320b9189edd28b6ff9070801a487a317f3d2b506
Cemag-pcp <109666229+Cemag-pcp@users.noreply.github.com>
2022-12-21 15:58:39 -0300

    Update app_ap.py

--- app_ap.py
+++ app_ap.py
@@ -7,7 +7,8 @@ from st_aggrid import AgGrid
 from st_aggrid.grid_options_builder import GridOptionsBuilder
 import time
 from PIL import Image
-from datetime import date
+from datetime import datetime
+from datetime import timedelta
 import datetime
 import numpy as np
 
@@ -16,7 +17,7 @@ import numpy as np
 # Connect to Google Sheets
 # ======================================= #
 
-st.markdown("<h1 style='text-align: center; font-size:60px; color: Black'>Apontamento de produção</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; font-size:60px; color: White'>Apontamento de produção</h1>", unsafe_allow_html=True)
 
 with st.sidebar:
     image = Image.open('logo-cemagL.png')
@@ -48,7 +49,7 @@ def load_datas():
     list2 = wks1.get_all_records()
     table1 = pd.DataFrame(list2)
     
-    #n_op = '23/12/2022'
+    #n_op = '21/12/2022'
     
     #n_op = st.sidebar.selectbox('Selecione o código da ordem:', lista_unicos)
     
@@ -69,58 +70,84 @@ def consultar(n_op,table1,table):
         
         filter_ = filter_.rename(columns={'DESCRICAO':'PEÇA'})
         filter_['QT. PRODUZIDA'] = 0
-        filter_ = filter_[['CODIGO', 'PEÇA', 'QT_ITENS', 'COR', 'QT. PRODUZIDA', 'DATA DA CARGA']]
+        filter_['TIPO'] = ''
+        filter_['COR'] = ''
+        filter_['UNICO'] = filter_['CODIGO'] + n_op
+        
+        for i in range(len(filter_)):    
+            filter_['COR'][i] = filter_['CODIGO'][i][6:]
+            
+        filter_ = filter_[['UNICO','CODIGO', 'PEÇA', 'QT_ITENS', 'COR', 'QT. PRODUZIDA', 'DATA DA CARGA']]
 
-        df3 = tab2[['CODIGO','CAMBÃO']]        
-        df3 = (df3[df3['CAMBÃO'] != ''])
+        df3 = tab2[['CODIGO','CAMBÃO','TIPO']]        
+        df3 = df3[df3['CAMBÃO'] != '']
+        df3 = df3[df3['TIPO'] != '']
         df3 = pd.merge(filter_,df3, on=['CODIGO'], how='left').drop_duplicates(subset=['CODIGO'])
                 
         qt_total = tab2[['CODIGO','QT APONT.']].groupby(['CODIGO']).sum().reset_index()        
         qt_total.drop(qt_total[qt_total['QT APONT.'] == 0].index, inplace=True)
         
         df3 = pd.merge(df3, qt_total, on=['CODIGO'], how='left')
-        
         df3 = df3.replace(np.nan,0)
             
-        #df3 = df3.drop(['QT. PRODUZIDA', 'QT APONT.'], axis=1)
-        #table_geral = pd.merge(df3, qt_total, how = 'inner', on = 'CODIGO' )
-        #table_geral['QT. PRODUZIDA'] = 0
         table_geral = df3
-        table_geral = table_geral[['CODIGO', 'PEÇA', 'QT_ITENS','COR','QT. PRODUZIDA','QT APONT.', 'CAMBÃO']]
+        table_geral = table_geral[['UNICO','CODIGO', 'PEÇA', 'QT_ITENS','COR','QT. PRODUZIDA','QT APONT.', 'CAMBÃO', 'TIPO']]
         
     else:
         
-        table_geral = filter_[['CODIGO', 'DESCRICAO', 'QT_ITENS', 'COR', 'QT. PRODUZIDA']]
+        filter_['COR'] = ''
+        
+        for i in range(len(filter_)):    
+            filter_['COR'][i] = filter_['CODIGO'][i][6:]
+        
+        filter_['UNICO'] = filter_['CODIGO'] + n_op
+        table_geral = filter_[['UNICO','CODIGO', 'DESCRICAO', 'QT_ITENS', 'COR', 'QT. PRODUZIDA']]
         table_geral['QT APONT.'] = 0
         table_geral['CAMBÃO'] = ''
-    
+        table_geral['TIPO'] = ''
+     
+# =============================================================================
+#     list_codigos = table_geral['CODIGO'].unique()
+#     
+#     filter_ = filter_.rename(columns={'DATA DA CARGA':'DATA_DA_CARGA'})
+#     df4 = filter_.query('CODIGO in @list_codigos and DATA_DA_CARGA == @n_op')
+# =============================================================================
+
     gb = GridOptionsBuilder.from_dataframe(table_geral)
-    gb.configure_default_column(min_column_width=30)
+    gb.configure_default_column(min_column_width=110)
     gb.configure_column('QT. PRODUZIDA', editable=True)
+    gb.configure_column('TIPO', editable=True)
     gb.configure_column('CAMBÃO', editable=True)
     grid_options = gb.build()
 
     grid_response = AgGrid(table_geral,
-                           gridOptions=grid_options,
-                           height=400,
-                           width='100%',
-                           data_return_mode='AS_INPUT',
-                           update_mode='MANUAL',
-                           try_to_convert_back_to_original_types = False,
-                           fit_columns_on_grid_load = True,
-                           theme='streamlit',
-                           )    
+                            gridOptions=grid_options,
+                            data_return_mode='AS_INPUT',
+                            width='100%',
+                            height=500,
+                            update_mode='MANUAL',
+                            try_to_convert_back_to_original_types = False,
+                            fit_columns_on_grid_load = True,
+                            theme='streamlit',
+                            )    
+    
     filter_new = grid_response['data']    
     
+    from datetime import datetime
+
     salvar = st.button("Salvar")
     filter_new['DATA DA CARGA'] = n_op
-    
+    filter_new['DATA FINALIZADA'] = datetime.now().strftime('%d/%m/%Y')
+
     if salvar:  
         
+        filter_new['UNICO'] = filter_new['CODIGO'] + n_op
         filter_new = filter_new.replace({'QT. PRODUZIDA':{'':0}})
         filter_new = filter_new.drop(columns={'QT APONT.'})
         filter_new['QT. PRODUZIDA'] = filter_new['QT. PRODUZIDA'].astype(int)
-        filter_new = filter_new.loc[(filter_new['QT. PRODUZIDA'] > 0)]
+        filter_new = filter_new.loc[(filter_new['CAMBÃO'] != '')]
+        filter_new = filter_new.loc[(filter_new['CAMBÃO'] != 0)]
+
         filter_new = filter_new.values.tolist()
         sh1.values_append('geral', {'valueInputOption': 'RAW'}, {'values': filter_new})
         
@@ -134,7 +161,7 @@ with st.sidebar:
         n_op = n_op.strftime('%d/%m/%Y')
         columns = st.columns((2, 1, 2))
         button_pressed = columns[1].form_submit_button('Ok')
-
+        
 if n_op != '':
     sh1, n_op, table1, table = load_datas()
     consultar(n_op,table1,table)
